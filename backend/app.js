9d68096e6d3149b4bdc6846d2d2b997365b85a01
Mikko <realmiks27@gmail.com>
2024-09-11 11:59:53 -0700

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 const s2023Router = require('./routes/s2023')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/2023', s2023Router)
 app.get('/', (req, res) => res.send('hi miks'))
