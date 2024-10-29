630d958e4024e4130f21406a0efb55042981940e
Mikko <realmiks27@gmail.com>
2024-10-29 10:51:47 -0700

    add connection to postgres db

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from datetime import datetime
+from nba_api.stats.endpoints import PlayerCareerStats
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import PlayerDashboardByYearOverYear
+import models
+from db import engine
+models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
+
+
 @app.get("/")
 def getIndex():
-    return {"Hello": "miks"}
+    return {"hello": "hi"}
+
+@app.get("/{name}")
+def getName(name: str):
+    return {"hello": f"hi there {name}"}
+
+
