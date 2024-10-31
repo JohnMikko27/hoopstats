13ccdba45db78f05d4a0bd8ce44235df272928e7
Mikko <realmiks27@gmail.com>
2024-10-30 17:25:09 -0700

    create Player and Season tables

---
+++ config/models.py
@@ -0,0 +1,49 @@
+from config.db import Base
+from sqlalchemy import Column, Integer, String, TIMESTAMP, ForeignKey, Float
+
+
+class Player(Base):
+    __tablename__ = "Player"
+
+    player_id = Column(Integer, primary_key=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    full_name = Column(String, nullable=False)
+    birthdate = Column(TIMESTAMP, nullable=False)
+    height = Column(String, nullable=False)
+    weight = Column(Integer, nullable=False)
+    years_in_league = Column(Integer, nullable=False)
+
+
+class Season(Base):
+    __tablename__ = "Season"
+
+    season_id = Column(Integer, primary_key=True, nullable=False)
+    player_id = Column(Integer, ForeignKey("Player.player_id"))
+    year = Column(String, nullable=False)
+    
+    GP = Column(Integer)
+    MIN = Column(Integer)
+    PTS = Column(Integer)
+    REB = Column(Integer)
+    AST = Column(Integer)
+    STL = Column(Integer)
+    BLK = Column(Integer)
+    FGM = Column(Integer)
+    FGA = Column(Integer)
+    FG_PCT = Column(Float)
+    FG3M = Column(Integer)
+    FG3A = Column(Integer)
+    FG3_PCT = Column(Float)
+    FTM = Column(Integer)
+    FTA = Column(Integer)
+    FT_PCT = Column(Float)
+    TS_PCT = Column(Float)
+    OREB = Column(Integer)
+    DREB = Column(Integer)
+    TOV = Column(Integer)
+    PF = Column(Integer)
+
+
+
+    
--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 from fastapi import FastAPI
 from datetime import datetime
+import config.models as models
+from config.db import engine
+models.Base.metadata.create_all(bind=engine)
+
 from nba_api.stats.endpoints import PlayerCareerStats
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import PlayerDashboardByYearOverYear
-import models
-from db import engine
-models.Base.metadata.create_all(bind=engine)
+from nba_api.stats.endpoints import CommonPlayerInfo
 
 
 app = FastAPI()
 
+player = players.find_players_by_full_name("leBron JaMeS")[0]
+player2 = players.find_players_by_full_name("jrue holiday")[0]
+player3 = players.find_players_by_full_name("jaylen brown")[0]
+print(player)
+player_stats = PlayerCareerStats(player_id=player['id']).career_totals_regular_season.get_dict()
+# print(player_stats)
 
+# getting year over year stats
+season = '2020-21'  # Specify the season
+player_dashboard = PlayerDashboardByYearOverYear(player_id=player['id'], season=season)
+years_played = len(player_dashboard.get_dict()['resultSets'][1]['rowSet'])
+stats_data = player_dashboard.get_dict()#['resultSets'][1]['rowSet'][8-years_played]
+
+# getting common player info
+player_info = CommonPlayerInfo(player_id=player3['id']).get_dict()
 
 @app.get("/")
 def getIndex():
     return {"hello": "hi"}
 
-@app.get("/{name}")
-def getName(name: str):
-    return {"hello": f"hi there {name}"}
+@app.get("/player")
+def getPlayer():
+    return { "hello": player_info}
+
+@app.get("/career")
+def getCareer():
+    return {"hello": stats_data}
 
 
