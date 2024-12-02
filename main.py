750d4faf90413124dfb25b4bcb521f03853ecf9a
Mikko <realmiks27@gmail.com>
2024-12-01 17:23:13 -0800

    add create extension command

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
 from .config import models as models
 from .config.db import engine
 from .routes.players import router as players_router
 from .routes.stats import router as stats_router
 
+def create_unaccent_extension():
+    with engine.connect() as connection:
+        connection.execute(text("CREATE EXTENSION IF NOT EXISTS unaccent;"))
+        connection.commit()
+
 app = FastAPI()
+
+create_unaccent_extension()
 models.Base.metadata.create_all(bind=engine)
 
 origins = [
