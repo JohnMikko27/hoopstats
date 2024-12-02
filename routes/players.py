3d75095c9209b294411097e7f0d714f481a9f587
Mikko <realmiks27@gmail.com>
2024-12-01 17:00:37 -0800

    remove bug when querying name with accents

--- routes/players.py
+++ routes/players.py
@@ -5,6 +5,7 @@ from nba_api.stats.endpoints import CommonPlayerInfo
 from sqlalchemy import func
 from ..config.db import get_db
 from ..config import models
+from ..utils.utils import remove_accents, get_player_without_accents
 
 router = APIRouter()
 
@@ -15,22 +16,22 @@ def fetch_player_info(name: str, db: Session):
     indexes = { "PERSON_ID": 0, "FIRST_NAME": 1, "LAST_NAME": 2, "DISPLAY_FIRST_LAST": 3,
             "BIRTHDATE": 7, "HEIGHT": 11, "WEIGHT": 12, "SEASON_EXP": 13}
     # check if player in db
-    player_info = db.query(models.Player).filter(func.lower(models.Player.full_name)==name.lower()).first()
+    player_info = db.query(models.Player).filter(func.lower((func.unaccent(models.Player.full_name)))
+                                                 ==remove_accents(name.lower())).first()
     if player_info:
         return player_info
     
     # check if player exists in nba_api
-    player_exists = players.find_players_by_full_name(name)[0] if players.find_players_by_full_name(name) else None
+    player_exists = get_player_without_accents(name)
     if player_exists:
         player = CommonPlayerInfo(player_id=player_exists['id']).get_dict()["resultSets"]
-        
         rowSet = player[0]['rowSet'][0]
         player_headshot = get_player_headshot_url(rowSet[indexes["PERSON_ID"]])
         player_info = models.Player(
             player_id=rowSet[indexes["PERSON_ID"]], 
-            first_name=rowSet[indexes["FIRST_NAME"]],
-            last_name=rowSet[indexes["LAST_NAME"]], 
-            full_name=rowSet[indexes["DISPLAY_FIRST_LAST"]],
+            first_name=(rowSet[indexes["FIRST_NAME"]]),
+            last_name=(rowSet[indexes["LAST_NAME"]]), 
+            full_name=(rowSet[indexes["DISPLAY_FIRST_LAST"]]),
             birthdate=rowSet[indexes["BIRTHDATE"]], 
             height=rowSet[indexes["HEIGHT"]],
             weight=rowSet[indexes["WEIGHT"]], 
